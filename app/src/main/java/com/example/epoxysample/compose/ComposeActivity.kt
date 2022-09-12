ab133148dd1e19385b57c11a844965d8dd3b3d5d
HeeGyeong <fhskf94kr@naver.com>
2022-09-13 00:41:13 +0900

    temp commit

---
+++ app/src/main/java/com/example/epoxysample/compose/ComposeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.epoxysample.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.animation.ExperimentalAnimationApi
+
+@ExperimentalAnimationApi
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*setContent {
+            SetSystemUI()
+            AppbarSample("AppBar Main")
+        }*/
+    }
+}
